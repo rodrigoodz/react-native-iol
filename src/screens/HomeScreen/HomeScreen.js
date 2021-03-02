bda9f3ed4242d59a1a55ea388d5674f6da38e611
Rodrigo Ortiz de Zarate <rodrigoodz_6@hotmail.com>
2021-03-02 16:16:41 -0300

    implementado fetch en HomeScreen

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,10 +1,12 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import {
   StyleSheet,
   Text,
+  View,
   ScrollView,
   TouchableOpacity,
   StatusBar,
+  ActivityIndicator,
 } from "react-native";
 
 import Title from "../../components/Title";
@@ -14,127 +16,42 @@ import { RefreshControl } from "react-native";
 import UpdateController from "./UpdateController";
 import { Context as AuthContext } from "../../context/AuthContext";
 
-const data = {
-  cuentas: [
-    {
-      numero: "200003",
-      tipo: "inversion_Argentina_Pesos",
-      moneda: "peso_Argentino",
-      disponible: 404.64,
-      comprometido: 0.0,
-      saldo: 404.64,
-      titulosValorizados: 563.25,
-      total: 967.89,
-      margenDescubierto: 0.0,
-      saldos: [
-        {
-          liquidacion: "inmediato",
-          saldo: 404.64,
-          comprometido: 0.0,
-          disponible: 404.64,
-          disponibleOperar: 404.64,
-        },
-        {
-          liquidacion: "hrs24",
-          saldo: 0.0,
-          comprometido: 0.0,
-          disponible: 0.0,
-          disponibleOperar: 404.64,
-        },
-        {
-          liquidacion: "hrs48",
-          saldo: 0.0,
-          comprometido: 0.0,
-          disponible: 0.0,
-          disponibleOperar: 404.64,
-        },
-        {
-          liquidacion: "hrs72",
-          saldo: 0.0,
-          comprometido: 0.0,
-          disponible: 0.0,
-          disponibleOperar: 404.64,
-        },
-        {
-          liquidacion: "masHrs72",
-          saldo: 0.0,
-          comprometido: 0.0,
-          disponible: 0.0,
-          disponibleOperar: 0.0,
-        },
-      ],
-      estado: "operable",
-    },
-    {
-      numero: "200003",
-      tipo: "inversion_Argentina_Dolares",
-      moneda: "dolar_Estadounidense",
-      disponible: 0.0,
-      comprometido: 0.0,
-      saldo: 0.0,
-      titulosValorizados: 0.0,
-      total: 0.0,
-      margenDescubierto: 0.0,
-      saldos: [
-        {
-          liquidacion: "inmediato",
-          saldo: 0.0,
-          comprometido: 0.0,
-          disponible: 0.0,
-          disponibleOperar: 0.0,
-        },
-        {
-          liquidacion: "hrs24",
-          saldo: 0.0,
-          comprometido: 0.0,
-          disponible: 0.0,
-          disponibleOperar: 0.0,
-        },
-        {
-          liquidacion: "hrs48",
-          saldo: 0.0,
-          comprometido: 0.0,
-          disponible: 0.0,
-          disponibleOperar: 0.0,
-        },
-        {
-          liquidacion: "hrs72",
-          saldo: 0.0,
-          comprometido: 0.0,
-          disponible: 0.0,
-          disponibleOperar: 0.0,
-        },
-        {
-          liquidacion: "masHrs72",
-          saldo: 0.0,
-          comprometido: 0.0,
-          disponible: 0.0,
-          disponibleOperar: 0.0,
-        },
-      ],
-      estado: "operable",
-    },
-  ],
-  estadisticas: [
-    {
-      descripcion: "Anterior",
-      cantidad: 0,
-      volumen: 0.0,
-    },
-    {
-      descripcion: "Actual",
-      cantidad: 0,
-      volumen: 0.0,
-    },
-  ],
-  totalEnPesos: 967.89,
-};
-
 const ScreenInicio = ({ navigation }) => {
   const [refreshing, setRefreshing] = useState(false);
-  const { state } = useContext(AuthContext);
 
-  console.log(state);
+  const [accountStatus, setAccountStatus] = useState(null);
+  const {
+    state: { access_token },
+  } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchAccountStatus = async () => {
+      try {
+        const response = await fetch(
+          "https://api.invertironline.com/api/v2/estadocuenta",
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${access_token}`,
+            },
+          }
+        );
+        let data = await response.json();
+        if (!data.message) {
+          setAccountStatus(data);
+        } else {
+          // console.log("Error");
+          navigation.reset({
+            index: 0,
+            routes: [{ name: "SignIn" }],
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAccountStatus();
+  }, []);
 
   const onRefresh = () => {
     setRefreshing(true);
@@ -144,55 +61,69 @@ const ScreenInicio = ({ navigation }) => {
     }, 2000);
   };
 
-  const {
-    totalEnPesos,
-    estadisticas,
-    cuentas: [
-      {
-        titulosValorizados,
-        disponible,
-        comprometido,
-        margenDescubierto,
-        saldos: [, hrs24, hrs48, hrs72, masHrs72],
-      },
-    ],
-  } = data;
-
-  return (
-    <ScrollView
-      style={styles.container}
-      showsVerticalScrollIndicator={false}
-      refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-      }
-    >
-      <Title textTitle="Cuenta de inversión Argentina" />
-      <BalanceInfo
-        data={{
-          totalEnPesos,
+  if (accountStatus) {
+    const {
+      totalEnPesos,
+      estadisticas,
+      cuentas: [
+        {
           titulosValorizados,
           disponible,
           comprometido,
           margenDescubierto,
-        }}
-      />
-      <TouchableOpacity
-        onPress={() =>
-          navigation.navigate("Estadisticas", { statistics: estadisticas })
+          saldos: [, hrs24, hrs48, hrs72, masHrs72],
+        },
+      ],
+    } = accountStatus;
+    return (
+      <ScrollView
+        style={styles.container}
+        showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
         }
-        style={{ alignSelf: "flex-start" }}
       >
-        <Text style={styles.buttonStyle}>Estadísticas</Text>
-      </TouchableOpacity>
+        <Title textTitle="Cuenta de inversión Argentina" />
+        <BalanceInfo
+          data={{
+            totalEnPesos,
+            titulosValorizados,
+            disponible,
+            comprometido,
+            margenDescubierto,
+          }}
+        />
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate("Estadisticas", { statistics: estadisticas })
+          }
+          style={{ alignSelf: "flex-start" }}
+        >
+          <Text style={styles.buttonStyle}>Estadísticas</Text>
+        </TouchableOpacity>
 
-      <CollapseItem title="a 24hs" data={hrs24} />
-      <CollapseItem title="a 48hs" data={hrs48} />
-      <CollapseItem title="a 72hs" data={hrs72} />
-      <CollapseItem title="a +72hs" data={masHrs72} />
-      <UpdateController />
-      <StatusBar />
-    </ScrollView>
-  );
+        <CollapseItem title="a 24hs" data={hrs24} />
+        <CollapseItem title="a 48hs" data={hrs48} />
+        <CollapseItem title="a 72hs" data={hrs72} />
+        <CollapseItem title="a +72hs" data={masHrs72} />
+        <UpdateController />
+        <StatusBar />
+      </ScrollView>
+    );
+  } else {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#4834D4",
+        }}
+      >
+        <ActivityIndicator size={60} color="white" />
+      </View>
+    );
+  }
 };
 
 const styles = StyleSheet.create({
