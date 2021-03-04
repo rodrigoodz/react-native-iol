faa22d8b72d430523e0766e505bdcd74062874f4
Rodrigo Ortiz de Zarate <rodrigoodz_6@hotmail.com>
2021-03-04 19:06:23 -0300

    cambiado estilo de ThreeColumnHeader

--- src/components/ThreeColumnHeader.js
+++ src/components/ThreeColumnHeader.js
@@ -23,14 +23,14 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: "row",
     justifyContent: "space-between",
-    backgroundColor: "#254f70",
+    backgroundColor: "white",
     alignItems: "center",
-    borderRadius: 3,
+    borderRadius: 10,
     marginBottom: 5,
   },
   text: {
     fontSize: 16,
-    color: "white",
+    color: "black",
     fontFamily: "SairaBold",
   },
 });
