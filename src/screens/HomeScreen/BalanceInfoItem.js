08798ad3982862717ad7fbc253ef6741ded01e1e
Rodrigo Ortiz de Zarate <rodrigoodz_6@hotmail.com>
2021-03-05 11:19:22 -0300

    estilos BalanceInfoItem agrupado en stylesheet

--- src/screens/HomeScreen/BalanceInfoItem.js
+++ src/screens/HomeScreen/BalanceInfoItem.js
@@ -1,33 +1,26 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 const BalanceInfoItem = ({ font_size, text_info, text_amount }) => {
   return (
     <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-      <Text
-        style={{
-          color: "rgba(255,255,255,0.8)",
-          fontSize: font_size,
-          padding: 5,
-          fontSize: 15,
-          fontFamily: "SairaLight",
-        }}
-      >
+      <Text style={[styles.textStyle, { fontSize: font_size }]}>
         {text_info}
       </Text>
-      <Text
-        style={{
-          color: "rgba(255,255,255,0.8)",
-          fontSize: font_size,
-          padding: 5,
-          fontSize: 15,
-          fontFamily: "SairaLight",
-        }}
-      >
+      <Text style={[styles.textStyle, { fontSize: font_size }]}>
         {text_amount}
       </Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  textStyle: {
+    color: "rgba(255,255,255,0.8)",
+    padding: 5,
+    fontSize: 15,
+    fontFamily: "SairaLight",
+  },
+});
 
 export default BalanceInfoItem;
