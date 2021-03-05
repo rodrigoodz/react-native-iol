dcdf1f51cf918b703e6869736a2b59df5d2ea07e
Rodrigo Ortiz de Zarate <rodrigoodz_6@hotmail.com>
2021-03-04 21:25:53 -0300

    cambiado diseño pantalla BuySellScreen

--- src/screens/BuySellScreen.js
+++ src/screens/BuySellScreen.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import GradientContainer from "../components/GradientContainer";
 import Title from "../components/Title";
 
 const BuySellScreen = () => {
   return (
-    <View style={{ flex: 1, padding: 10, backgroundColor: "#131e31" }}>
-      <Title textTitle="Operar" />
+    <View style={{ flex: 1, backgroundColor: "#131e31" }}>
+      <GradientContainer
+        firstColor="#2b5a7f"
+        secondColor="#193952"
+        padding={10}
+        borderBottomLeftRadius={30}
+        borderBottomRightRadius={30}
+        marginBottom={20}
+      >
+        <Title textTitle="Operar" />
+      </GradientContainer>
     </View>
   );
 };
