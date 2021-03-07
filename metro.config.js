5a4023bee38ebdabda4de5999342f2b8631e8b7c
Rodrigo Ortiz de Zarate <rodrigoodz_6@hotmail.com>
2021-03-06 21:38:41 -0300

    instalado @expo/metro-config

--- metro.config.js
+++ metro.config.js
@@ -1,21 +1,9 @@
-const { getDefaultConfig } = require("metro-config");
+const { getDefaultConfig } = require("@expo/metro-config");
 
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: false,
-        },
-      }),
-    },
-    resolver: {
-      sourceExts,
-      assetExts: [...assetExts, "hcscript"],
-    },
-  };
-})();
+const defaultConfig = getDefaultConfig(__dirname);
+
+module.exports = {
+  resolver: {
+    assetExts: [...defaultConfig.resolver.assetExts, "hcscript"],
+  },
+};
