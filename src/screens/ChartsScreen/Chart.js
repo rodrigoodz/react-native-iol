ced25360d47ff8a36c558b8f6ff77b0d2c9d94e3
Rodrigo Ortiz de Zarate <rodrigoodz_6@hotmail.com>
2021-03-10 20:35:07 -0300

    grafica implementada mediante victory

--- src/screens/ChartsScreen/Chart.js
+++ src/screens/ChartsScreen/Chart.js
@@ -1,88 +1,49 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import HighchartsReactNative from "@highcharts/highcharts-react-native";
+import {
+  VictoryChart,
+  VictoryTheme,
+  VictoryAxis,
+  VictoryLine,
+} from "victory-native";
 
 const Chart = ({ data, asset }) => {
   return (
     <View style={styles.container}>
-      <HighchartsReactNative
-        styles={styles.container}
-        options={{
-          title: {
-            text: `${asset}`,
-          },
-          yAxis: {
-            title: {
-              text: "Precio",
-            },
-            labels: {
-              format: "{value:,.0f}",
-            },
-            gridLineColor: "rgba(255,255,255,.2)",
-          },
-          chart: {
-            backgroundColor: "#111112",
-          },
-          tooltip: {
-            crosshairs: [true, true],
-            formatter: function () {
-              return (
-                "Fecha: " +
-                this.point.fecha +
-                "<br/>" +
-                "Cierre: " +
-                this.point.y +
-                "$" +
-                "<br/>" +
-                "Apertura " +
-                this.point.apertura +
-                "$" +
-                "<br/>" +
-                "Maximo " +
-                this.point.maximo +
-                "$" +
-                "<br/>" +
-                "Minimo " +
-                this.point.minimo +
-                "$" +
-                "<br/>"
-              );
-            },
-          },
-          legend: {
-            enabled: false,
-          },
-          credits: {
-            enabled: false,
-          },
-          xAxis: {
-            type: "datetime",
-            labels: {
-              enabled: false,
-              format: "{value:%Y-%m-%d}",
-              rotation: 45,
-              align: "left",
-            },
-          },
-          series: [
-            {
-              data: data.reverse().map((d) => ({
-                y: d.ultimoPrecio,
-                fecha: d.fechaHora.split("T"),
-                apertura: d.apertura,
-                maximo: d.maximo,
-                minimo: d.minimo,
-              })),
-              pointStart: Date.UTC(
-                data.reverse()[0].fechaHora.substring(0, 4),
-                data.reverse()[0].fechaHora.substring(5, 7),
-                data.reverse()[0].fechaHora.substring(8, 10)
-              ),
-              pointInterval: 24 * 3600 * 1000,
-            },
-          ],
-        }}
-      />
+      <VictoryChart
+        theme={VictoryTheme.material}
+        domainPadding={{ x: 2 }}
+        scale={{ x: "time" }}
+      >
+        <VictoryAxis
+          dependentAxis
+          style={{
+            axis: { stroke: "#ccc" },
+            tickLabels: { padding: 10 },
+            axisLabel: { padding: 2 },
+            grid: { stroke: "rgba(255,255,255,.1)" },
+          }}
+          label="Precio"
+        />
+        <VictoryAxis
+          tickFormat={(t) => `${t.getDate()}/${t.getMonth() + 1}`}
+          style={{
+            axis: { stroke: "#ccc" },
+            tickLabels: { angle: 0, padding: 2 },
+            grid: { stroke: "rgba(255,255,255,.1)" },
+          }}
+        />
+        <VictoryLine
+          style={{
+            data: { stroke: "#0091c8" },
+            parent: { border: "1px solid #ccc" },
+          }}
+          data={data.map((d) => ({
+            x: new Date(d.fechaHora),
+            y: d.ultimoPrecio,
+          }))}
+        />
+      </VictoryChart>
     </View>
   );
 };
@@ -92,9 +53,9 @@ export default Chart;
 const styles = StyleSheet.create({
   container: {
     justifyContent: "center",
-    // flex: 1,
     height: 300,
     borderRadius: 5,
     marginHorizontal: 5,
+    borderWidth: 1,
   },
 });
